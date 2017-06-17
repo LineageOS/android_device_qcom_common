9658dfa7d1e3834e70903530dd93cf4977c1de03
Luca Stefani <luca020400@lineageos.org>
2017-06-17 22:59:44 +0200

    power-8952: Enable MPCTLV3
    Change-Id: Ie4ac12e7607ee0ee278135aaac8a567fe5252eaa

--- power/Android.mk
+++ power/Android.mk
@@ -47,6 +47,7 @@ endif
 
 ifeq ($(call is-board-platform-in-list, msm8952), true)
 LOCAL_SRC_FILES += power-8952.c
+LOCAL_CFLAGS += -DMPCTLV3
 endif
 
 ifeq ($(call is-board-platform-in-list, msm8937 msm8953), true)
