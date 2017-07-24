d1dacc6c79481f2f1e720a2cb14401a3593c4e26
Luca Stefani <luca020400@lineageos.org>
2017-07-24 19:22:35 +0200

    power: Add msm8998 hal
    Change-Id: If6dca3a78f0b9f7f1d7bd25f2a1cd84ac3a258db

--- power/Android.mk
+++ power/Android.mk
@@ -71,6 +71,11 @@ LOCAL_SRC_FILES += power-8996.c
 LOCAL_CFLAGS += -DMPCTLV3
 endif
 
+ifeq ($(call is-board-platform-in-list, msm8998), true)
+LOCAL_SRC_FILES += power-8998.c
+LOCAL_CFLAGS += -DMPCTLV3
+endif
+
 endif  #  End of board specific list
 
 ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
